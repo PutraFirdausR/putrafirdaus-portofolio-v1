7798df32d873b5916528212d73a4767a50bc2cb9
PutraFirdausR <putrafirdaus6373@gmail.com>
2024-11-06 13:35:37 +0700

    perbaikan

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -17,26 +17,10 @@
     >
       <div
         data-aos="fade-up"
-        class="card bg-base-300 w-96 rounded-t-lg shadow-xl group z-10 hover:brightness-110 hover:shadow-neutral-content/50 hover:shadow-xl"
+        class="card bg-base-300 w-96 rounded-t-lg shadow-xl group z-10 hover:shadow-neutral-content/50 hover:shadow-xl"
       >
         <figure class="relative font-Poppins">
           <img src="/src/assets/Movie.png" alt="Movie-App" />
-          <div
-            class="absolute inset-0 font-Poopins bg-base-300/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col gap-2 items-center justify-center"
-          >
-            <a
-              href="https://github.com/PutraFirdausR/movie-web-app"
-              target="_blank"
-              class="btn btn-primary text-white rounded"
-              >Visit Github Repo</a
-            >
-            <a
-              href="https://kenzomovie.vercel.app/"
-              class="btn btn-secondary text-white rounded"
-              target="_blank"
-              >Visit Website</a
-            >
-          </div>
         </figure>
         <div class="card-body z-10 hover:">
           <h2
@@ -50,17 +34,52 @@
             Apart from that, there are popular, newest and trending film
             features in real time.
           </p>
+          <div class="card-actions justify-end pb-2">
+            <div class="badge badge-outline">HTML</div>
+            <div class="badge badge-outline">CSS</div>
+            <div class="badge badge-outline">JavaScript</div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <a
+              href="https://github.com/PutraFirdausR/movie-web-app"
+              target="_blank"
+              class="btn btn-primary text-white rounded"
+              >Visit Github Repo</a
+            >
+            <a
+              href="https://kenzomovie.vercel.app/"
+              class="btn btn-secondary text-white rounded"
+              target="_blank"
+              >Visit Website</a
+            >
+          </div>
         </div>
       </div>
       <div
         data-aos="fade-up"
-        class="card bg-base-300 w-96 shadow-xl z-10 group rounded-t-lg hover:brightness-110 hover:shadow-neutral-content/50 hover:shadow-xl"
+        class="card bg-base-300 w-96 shadow-xl z-10 group rounded-t-lg hover:shadow-neutral-content/50 hover:shadow-xl"
       >
         <figure class="relative">
           <img src="/src/assets/Residence.png" alt="Residence-App" />
-          <div
-            class="absolute font-Poppins inset-0 bg-base-300/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col gap-2 items-center justify-center"
-          >
+        </figure>
+        <div class="card-body z-10">
+          <h2 class="card-title text-white font-medium font-Poppins">
+            Firdaus Residence
+          </h2>
+          <p class="text-[#87a5a5] font-Poppins">
+            Property Web is an online platform that specifically provides
+            Complete information about various types of villas with facilities
+            complete. This site is designed to help users find villas that suits
+            their needs and preferences.
+          </p>
+          <div class="card-actions justify-end pb-2">
+            <div class="badge badge-outline">HTML</div>
+            <div class="badge badge-outline">CSS</div>
+            <div class="badge badge-outline">JavaScript</div>
+            <div class="badge badge-outline">GSAP</div>
+            <div class="badge badge-outline">AOS</div>
+          </div>
+          <div class="flex flex-col gap-2">
             <a
               href="https://github.com/PutraFirdausR/firdaus-residence"
               target="_blank"
@@ -74,28 +93,30 @@
               >Visit Website</a
             >
           </div>
-        </figure>
-        <div class="card-body z-10">
-          <h2 class="card-title text-white font-medium font-Poppins">
-            Firdaus Residence
-          </h2>
-          <p class="text-[#87a5a5] font-Poppins">
-            Property Web is an online platform that specifically provides
-            Complete information about various types of villas with facilities
-            complete. This site is designed to help users find villas that suits
-            their needs and preferences.
-          </p>
         </div>
       </div>
       <div
         data-aos="fade-up"
-        class="card bg-base-300 z-10 w-96 shadow-xl group rounded-t-lg hover:brightness-110 hover:shadow-neutral-content/50 hover:shadow-xl"
+        class="card bg-base-300 z-10 w-96 shadow-xl group rounded-t-lg hover:shadow-neutral-content/50 hover:shadow-xl"
       >
         <figure class="relative">
           <img src="/src/assets/LandingPage.png" alt="Landing-Page" />
-          <div
-            class="absolute inset-0 font-Poopins bg-base-300/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col gap-2 items-center justify-center"
-          >
+        </figure>
+        <div class="card-body z-10">
+          <h2 class="card-title text-white font-Poppins font-medium">
+            Landing Page
+          </h2>
+          <p class="text-[#87a5a5] font-Poppins">
+            This landing page is my first website that I used to have plans to
+            be a source of information for anime and manga fans Kimetsu No
+            Yaiba. Complete with About, Character, and features Contact. And
+            will be updated
+          </p>
+          <div class="card-actions justify-end pb-2">
+            <div class="badge badge-outline">HTML</div>
+            <div class="badge badge-outline">CSS</div>
+          </div>
+          <div class="flex flex-col gap-2">
             <a
               href="https://github.com/PutraFirdausR/Kimetsu-No-Yaiba"
               target="_blank"
@@ -109,35 +130,14 @@
               >Visit Website</a
             >
           </div>
-        </figure>
-        <div class="card-body z-10">
-          <h2 class="card-title text-white font-Poppins font-medium">
-            Landing Page
-          </h2>
-          <p class="text-[#87a5a5] font-Poppins">
-            This landing page is my first website that I used to have plans to
-            be a source of information for anime and manga fans Kimetsu No
-            Yaiba. Complete with About, Character, and features Contact. And
-            will be updated
-          </p>
         </div>
       </div>
       <div
         data-aos="fade-up"
-        class="card bg-base-300 z-10 w-96 shadow-xl group rounded-t-lg hover:brightness-110 hover:shadow-neutral-content/50 hover:shadow-xl"
+        class="card bg-base-300 z-10 w-96 shadow-xl group rounded-t-lg hover:shadow-neutral-content/50 hover:shadow-xl"
       >
         <figure class="relative">
           <img src="/src/assets/Quiz.png" alt="Quiz-App" />
-          <div
-            class="absolute inset-0 font-Poopins bg-base-300/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col gap-2 items-center justify-center"
-          >
-            <a
-              href="https://github.com/PutraFirdausR/quiz-web-app"
-              target="_blank"
-              class="btn btn-primary text-white rounded"
-              >Visit Github Repo</a
-            >
-          </div>
         </figure>
         <div class="card-body z-10">
           <h2 class="card-title text-white font-Poppins font-medium">
@@ -149,17 +149,45 @@
             providing assessments in addition. There are timer and registration
             features
           </p>
+          <div class="card-actions justify-end pb-2">
+            <div class="badge badge-outline">HTML</div>
+            <div class="badge badge-outline">CSS</div>
+            <div class="badge badge-outline">JavaScript</div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <a
+              href="https://github.com/PutraFirdausR/quiz-web-app"
+              target="_blank"
+              class="btn btn-primary text-white rounded"
+              >Visit Github Repo</a
+            >
+          </div>
         </div>
       </div>
       <div
         data-aos="fade-up"
-        class="card bg-base-300 w-96 z-10 shadow-xl group rounded-t-lg hover:brightness-110 hover:shadow-neutral-content/50 hover:shadow-xl"
+        class="card bg-base-300 w-96 z-10 shadow-xl group rounded-t-lg hover:shadow-neutral-content/50 hover:shadow-xl"
       >
         <figure class="relative">
           <img src="/src/assets/Portofolio.png" alt="Portofolio-Web" />
-          <div
-            class="absolute inset-0 font-Poopins bg-base-300/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col gap-2 items-center justify-center"
-          >
+        </figure>
+        <div class="card-body z-10">
+          <h2 class="card-title text-white font-Poppins font-medium">
+            Portofolio
+          </h2>
+          <p class="text-[#87a5a5] font-Poppins">
+            I built an interactive web portfolio that combines dynamic animation
+            with responsive design to showcase my work and skills. This project
+            was created using Vue.js technology, Tailwind CSS to support
+            responsive and modular design, and AOS.
+          </p>
+          <div class="card-actions justify-end pb-2">
+            <div class="badge badge-outline">Vuejs</div>
+            <div class="badge badge-outline">Tailwind</div>
+            <div class="badge badge-outline">Daisy UI</div>
+            <div class="badge badge-outline">AOS</div>
+          </div>
+          <div class="flex flex-col gap-2">
             <a
               href="https://github.com/PutraFirdausR/putrafirdaus-portofolio-v1"
               target="_blank"
@@ -173,35 +201,14 @@
               >Visit Website</a
             >
           </div>
-        </figure>
-        <div class="card-body z-10">
-          <h2 class="card-title text-white font-Poppins font-medium">
-            Portofolio
-          </h2>
-          <p class="text-[#87a5a5] font-Poppins">
-            I built an interactive web portfolio that combines dynamic animation
-            with responsive design to showcase my work and skills. This project
-            was created using Vue.js technology, Tailwind CSS to support
-            responsive and modular design, and AOS.
-          </p>
         </div>
       </div>
       <div
         data-aos="fade-up"
-        class="card bg-base-300 w-96 z-10 shadow-xl group rounded-t-lg hover:brightness-110 hover:shadow-neutral-content/50 hover:shadow-xl"
+        class="card bg-base-300 w-96 z-10 shadow-xl group rounded-t-lg hover:shadow-neutral-content/50 hover:shadow-xl"
       >
         <figure class="relative">
           <img src="/src/assets/ToDoList.png" alt="To-Do-List" />
-          <div
-            class="absolute inset-0 font-Poopins bg-base-300/80 opacity-0 group-hover:opacity-100 transition-all duration-300 flex flex-col gap-2 items-center justify-center"
-          >
-            <a
-              href="https://github.com/PutraFirdausR/to-do-list"
-              target="_blank"
-              class="btn btn-primary text-white rounded"
-              >Visit Github Repo</a
-            >
-          </div>
         </figure>
         <div class="card-body z-10">
           <h2 class="card-title text-white font-Poppins font-medium">
@@ -213,6 +220,19 @@
             new tasks, Mark tasks as completed, delete task,storage.This project
             is my first step in learning Front-End web development
           </p>
+          <div class="card-actions justify-end pb-2">
+            <div class="badge badge-outline">HTML</div>
+            <div class="badge badge-outline">CSS</div>
+            <div class="badge badge-outline">JavaScript</div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <a
+              href="https://github.com/PutraFirdausR/to-do-list"
+              target="_blank"
+              class="btn btn-primary text-white rounded"
+              >Visit Github Repo</a
+            >
+          </div>
         </div>
       </div>
     </div>
